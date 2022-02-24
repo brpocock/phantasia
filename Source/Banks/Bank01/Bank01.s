19cb4b6b43f500d30688348ebb2699f627f7d354
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-24 00:13:11 -0500

    interleave

--- Source/Banks/Bank01/Bank01.s
+++ Source/Banks/Bank01/Bank01.s
@@ -11,6 +11,9 @@ BankEntry:
           brk
 
           .include "Font.320A.8.s"
+          * = Font + 64                 ; Interleave BigFont between Font
           .include "BigFont.320A.16.s"
+          ;; XXX * = Font + 192
+          ;; XXX Could include another 64 bytes of interleaved data here
 
           .include "EndBank.s"
