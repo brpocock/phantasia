e712c4a8796b08f4f7000b1ce8f1378ec1f97975
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-25 14:40:37 -0500

    big font working (now the small text doesn't)

--- Source/Common/StartBank.s
+++ Source/Common/StartBank.s
@@ -53,43 +53,12 @@
 	.cdef "←←", $3e
 	.cdef "→→", $3f
 
-	.enc "bigfont"
-          ;; characters are 2 bytes wide so each pointer is doubled
-	.cdef "09", 0
-	.cdef "az", $0a * 2
-	.cdef "AZ", $0a * 2
-	.cdef "  ", $24 * 2
-	.cdef ",,", $25 * 2
-	.cdef "..", $26 * 2
-	.cdef "??", $27 * 2
-	.cdef "!!", $28 * 2
-	.cdef "//", $29 * 2
-	.cdef "&&", $2a * 2
-	.cdef "++", $2b * 2
-	.cdef "--", $2c * 2
-	.cdef "××", $2d * 2
-	.cdef "÷÷", $2e * 2
-	.cdef "==", $2f * 2
-	.cdef "““", $30 * 2
-	.cdef "””", $31 * 2
-	.cdef "’’", $32 * 2
-	.cdef "''", $32 * 2
-	.cdef "::", $33 * 2
-	.cdef ";;", $34 * 2
-          .cdef "……", $35 * 2       ; actually only the first two dots, follow with a .
-          ;; …
-	.cdef "©©", $3a * 2
-	.cdef "••", $3b * 2
-	.cdef "↑↑", $3c * 2
-	.cdef "↓↓", $3d * 2
-	.cdef "←←", $3e * 2
-	.cdef "→→", $3f * 2
-
           .enc "Unicode"
 
-bigtext:  .macro string
+bigptext:  .macro string
+          .byte len(\string)
           .for i := 0, i < len(\string), i += 1
-          .byte \string[i] * 2, 1+ \string[i] * 2
+          .byte \string[i] * 2
           .next
           .endm
           
--- Source/Routines/PublisherPrelude.s
+++ Source/Routines/PublisherPrelude.s
@@ -10,8 +10,9 @@ PublisherPrelude:	.block
           PreludeDLL = SysRAMHigh
           BlankDList = SysRAMHigh + $40
           TitleTextDList = SysRAMHigh + $50
-          SubtitleTextDList = SysRAMHigh + $50
-
+          SubtitleTextDList = SysRAMHigh + $70
+          CopyrightTextDList = SysRAMHigh + $80
+;;; 
           ;; Blank row display list is empty.
           .mva BlankDList, #0
           .mva BlankDList + 1, #0
@@ -25,47 +26,70 @@ PublisherPrelude:	.block
           .DLPalDynWidth 0, TitleTextString
           sta TitleTextDList, y
           iny
-          .mvayi TitleTextDList, #16
+          .mvayi TitleTextDList, # 32
 
-          .mvayi TitleTextDList, #0
-          .mvay TitleTextDList, #0
+          .mvayi TitleTextDList, # 0
+          .mvay TitleTextDList, # 0
 
           .mvayi SubtitleTextDList, #<SubtitleTextString+1
           .mvayi SubtitleTextDList, #DLExtMode(false, true)
           .mvayi SubtitleTextDList, #>SubtitleTextString+1
-          .DLPalDynWidth 0, SubtitleTextString
+          .DLPalDynWidth 1, SubtitleTextString
           sta SubtitleTextDList, y
           iny
-          .mvayi SubtitleTextDList, #16
+          .mvayi SubtitleTextDList, # 50
 
-          .mvayi SubtitleTextDList, #0
-          .mvay SubtitleTextDList, #0
-          
+          .mvayi SubtitleTextDList, # 0
+          .mvay SubtitleTextDList, # 0
+
+          .mvayi CopyrightTextDList, #<CopyrightTextString+1
+          .mvayi CopyrightTextDList, #DLExtMode(false, true)
+          .mvayi CopyrightTextDList, #>CopyrightTextString+1
+          .DLPalDynWidth 2, CopyrightTextString
+          sta CopyrightTextDList, y
+          iny
+          .mvayi CopyrightTextDList, # 50
+
+          .mvayi CopyrightTextDList, # 0
+          .mvay CopyrightTextDList, # 0
+;;; 
           ;; Display List List
           ldy # 0
 
           ldx # 7
 FillTopBlank:
-          .mvayi PreludeDLL, #10
+          .mvayi PreludeDLL, # 10
           .mvayi PreludeDLL, #>BlankDList
           .mvayi PreludeDLL, #<BlankDList
 
           dex
           bne FillTopBlank
 
+          .mvayi PreludeDLL, # 5 | DLLDLI
+          .mvayi PreludeDLL, #>BlankDList
+          .mvayi PreludeDLL, #<BlankDList
+
 TitleText:
-          .mvayi PreludeDLL, #15 | DLLDLI | DLLHoley16
+          .mvayi PreludeDLL, # 15
           .mvayi PreludeDLL, #>TitleTextDList
           .mvayi PreludeDLL, #<TitleTextDList
           
+          .mvayi PreludeDLL, # 5
+          .mvayi PreludeDLL, #>BlankDList
+          .mvayi PreludeDLL, #<BlankDList
+
+          .mvayi PreludeDLL, # 0 | DLLDLI
+          .mvayi PreludeDLL, #>BlankDList
+          .mvayi PreludeDLL, #<BlankDList
+
 SubtitleText:
-          .mvayi PreludeDLL, #7 | DLLDLI | DLLHoley8
+          .mvayi PreludeDLL, # 7
           .mvayi PreludeDLL, #>SubtitleTextDList
           .mvayi PreludeDLL, #<SubtitleTextDList
           
           ldx # 10
 FillBottomBlank:
-          .mvayi PreludeDLL, #15
+          .mvayi PreludeDLL, # 15
           .mvayi PreludeDLL, #>BlankDList
           .mvayi PreludeDLL, #<BlankDList
 
@@ -75,48 +99,48 @@ FillBottomBlank:
           .mvayi PreludeDLL, #10
           .mvayi PreludeDLL, #>BlankDList
           .mvayi PreludeDLL, #<BlankDList
-
-
+;;; 
           .WaitForVBlank
           ;; Set up Maria controls
-          .mva BACKGRND, #CoLu(COLYELLOW, $f)
-          .mva P0C1, #CoLu(COLGRAY, $0)
           .mva P0C2, #CoLu(COLGRAY, $f)
-          .mva P0C3, #CoLu(COLGRAY, $0)
+          .mva P1C2, #CoLu(COLGRAY, $f)
+          .mva P2C2, #CoLu(COLGRAY, $0)
           .mva DPPL, #<PreludeDLL
           .mva DPPH, #>PreludeDLL
+
+          ;; Duplicate this in the bottommost NMI routine as well
           .mvaw NMINext, NMISwitchToBigFont
+          .mva BACKGRND, #CoLu(COLBLUE, $8)
+          .mva CHARBASE, #>Font
           ;; Turn on the Maria
           .mva CTRL, #CTRLDMAEnable | CTRLRead320AC
 
           ;; XXX Hang
 Hang:
           jmp Hang
-
 ;;; 
-          
 NMISwitchToBigFont:
           .mvaw NMINext, NMISwitchToFont
           .mva BACKGRND, #CoLu(COLGREEN, $8)
           .mva CHARBASE, #>BigFont
+          .mva CTRL, #CTRLDMAEnable | CTRLRead320AC | CTRLCharWide
           rti
 
 NMISwitchToFont:
           .mvaw NMINext, NMISwitchToBigFont
           .mva BACKGRND, #CoLu(COLBLUE, $8)
           .mva CHARBASE, #>Font
+          .mva CTRL, #CTRLDMAEnable | CTRLRead320AC
           rti
-
 ;;; 
-          
-          .enc "minifont"
-
 TitleTextString:
-          .ptext "phantasia"
-          .fill 50, 0           ; XXX
+          .enc "minifont"
+          .bigptext "phantasia"
 
 SubtitleTextString:
+          .ptext "an adventure"
+
+CopyrightTextString:
           .ptext "© 2022 bruce-robert pocock"
 
-          .fill 50, 0           ; XXX
           .bend
