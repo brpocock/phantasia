1d4321e058e516e1102aff97c5d25b076925d512
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-03-22 23:28:42 -0400

    add map source files

---
+++ Source/Maps/Overworld Tiles.tsx
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Overworld Tiles" tilewidth="8" tileheight="16" tilecount="128" columns="16">
+ <image source="OverworldTiles.png" width="128" height="128"/>
+ <wangsets>
+  <wangset name="Water and Land" type="corner" tile="-1">
+   <wangcolor name="Land" color="#26a269" tile="-1" probability="1"/>
+   <wangcolor name="Water" color="#1a5fb4" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="19" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="20" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="32" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="33" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="35" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="36" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="48" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="49" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="50" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="51" wangid="0,2,0,1,0,2,0,1"/>
+   <wangtile tileid="52" wangid="0,1,0,2,0,1,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
