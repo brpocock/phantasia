22c3b627d41dc079eba65d7bf608d5470b604343
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-03-25 09:02:46 -0400

    working on tile sets

--- Source/Maps/Overworld Tiles.tsx
+++ Source/Maps/Overworld Tiles.tsx
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Overworld Tiles" tilewidth="8" tileheight="16" tilecount="128" columns="16">
  <image source="OverworldTiles.png" width="128" height="129"/>
+ <tile id="1" probability="0.25"/>
  <tile id="2">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="16"/>
@@ -26,6 +27,12 @@
    <object id="1" x="0" y="0" width="8" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="17">
+  <animation>
+   <frame tileid="17" duration="667"/>
+   <frame tileid="70" duration="667"/>
+  </animation>
+ </tile>
  <tile id="21">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="16"/>
@@ -41,13 +48,21 @@
    <object id="1" x="0" y="0" width="8" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="667"/>
+   <frame tileid="86" duration="667"/>
+  </animation>
+ </tile>
  <tile id="33">
   <properties>
    <property name="Water" type="bool" value="true"/>
   </properties>
+ </tile>
+ <tile id="34">
   <animation>
-   <frame tileid="33" duration="667"/>
-   <frame tileid="4" duration="667"/>
+   <frame tileid="34" duration="667"/>
+   <frame tileid="71" duration="667"/>
   </animation>
  </tile>
  <tile id="37">
@@ -64,6 +79,12 @@
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="16"/>
   </objectgroup>
+ </tile>
+ <tile id="49">
+  <animation>
+   <frame tileid="49" duration="667"/>
+   <frame tileid="87" duration="667"/>
+  </animation>
  </tile>
  <tile id="51">
   <objectgroup draworder="index" id="2">
@@ -85,6 +106,7 @@
    <object id="1" x="0" y="0" width="8" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="58" probability="0.25"/>
  <tile id="64">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="16"/>
@@ -117,21 +139,11 @@
    <object id="1" x="0" y="0" width="5.65705" height="13.657"/>
   </objectgroup>
  </tile>
- <tile id="114">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="115">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="16"/>
-  </objectgroup>
- </tile>
  <wangsets>
   <wangset name="Water and Land" type="corner" tile="-1">
    <wangcolor name="Land" color="#26a269" tile="-1" probability="1"/>
    <wangcolor name="Water" color="#1a5fb4" tile="-1" probability="1"/>
-   <wangcolor name="Dirt Road" color="#fcaf3e" tile="-1" probability="1"/>
+   <wangcolor name="Dirt" color="#fcaf3e" tile="-1" probability="1"/>
    <wangcolor name="Roof" color="#ef2929" tile="-1" probability="1"/>
    <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
@@ -141,37 +153,37 @@
    <wangtile tileid="8" wangid="0,1,0,3,0,1,0,1"/>
    <wangtile tileid="9" wangid="0,1,0,3,0,3,0,1"/>
    <wangtile tileid="10" wangid="0,1,0,1,0,3,0,1"/>
-   <wangtile tileid="16" wangid="0,1,0,2,0,1,0,1"/>
-   <wangtile tileid="17" wangid="0,1,0,2,0,2,0,1"/>
-   <wangtile tileid="18" wangid="0,1,0,1,0,2,0,1"/>
-   <wangtile tileid="19" wangid="0,2,0,1,0,2,0,2"/>
-   <wangtile tileid="20" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="16" wangid="0,3,0,2,0,3,0,3"/>
+   <wangtile tileid="17" wangid="0,3,0,2,0,2,0,3"/>
+   <wangtile tileid="18" wangid="0,3,0,3,0,2,0,3"/>
+   <wangtile tileid="19" wangid="0,2,0,3,0,2,0,2"/>
+   <wangtile tileid="20" wangid="0,2,0,2,0,3,0,2"/>
    <wangtile tileid="21" wangid="0,4,0,4,0,0,0,0"/>
    <wangtile tileid="22" wangid="0,4,0,4,0,4,0,4"/>
    <wangtile tileid="23" wangid="0,0,0,0,0,4,0,4"/>
    <wangtile tileid="24" wangid="0,3,0,3,0,1,0,1"/>
    <wangtile tileid="26" wangid="0,1,0,1,0,3,0,3"/>
-   <wangtile tileid="32" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="32" wangid="0,2,0,2,0,3,0,3"/>
    <wangtile tileid="33" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="34" wangid="0,1,0,1,0,2,0,2"/>
-   <wangtile tileid="35" wangid="0,1,0,2,0,2,0,2"/>
-   <wangtile tileid="36" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="34" wangid="0,3,0,3,0,2,0,2"/>
+   <wangtile tileid="35" wangid="0,3,0,2,0,2,0,2"/>
+   <wangtile tileid="36" wangid="0,2,0,2,0,2,0,3"/>
    <wangtile tileid="37" wangid="0,4,0,0,0,0,0,0"/>
    <wangtile tileid="38" wangid="0,4,0,0,0,0,0,4"/>
    <wangtile tileid="39" wangid="0,0,0,0,0,0,0,4"/>
    <wangtile tileid="40" wangid="0,3,0,1,0,1,0,1"/>
    <wangtile tileid="41" wangid="0,3,0,1,0,1,0,3"/>
    <wangtile tileid="42" wangid="0,1,0,1,0,1,0,3"/>
-   <wangtile tileid="48" wangid="0,2,0,1,0,1,0,1"/>
-   <wangtile tileid="49" wangid="0,2,0,1,0,1,0,2"/>
-   <wangtile tileid="50" wangid="0,1,0,1,0,1,0,2"/>
-   <wangtile tileid="51" wangid="0,2,0,1,0,2,0,1"/>
-   <wangtile tileid="52" wangid="0,1,0,2,0,1,0,2"/>
+   <wangtile tileid="48" wangid="0,2,0,3,0,3,0,3"/>
+   <wangtile tileid="49" wangid="0,2,0,3,0,3,0,2"/>
+   <wangtile tileid="50" wangid="0,3,0,3,0,3,0,2"/>
    <wangtile tileid="56" wangid="0,3,0,1,0,3,0,3"/>
    <wangtile tileid="57" wangid="0,3,0,3,0,1,0,3"/>
+   <wangtile tileid="58" wangid="0,3,0,3,0,3,0,3"/>
    <wangtile tileid="66" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="72" wangid="0,1,0,3,0,3,0,3"/>
    <wangtile tileid="73" wangid="0,3,0,3,0,3,0,1"/>
+   <wangtile tileid="74" wangid="0,3,0,3,0,3,0,3"/>
    <wangtile tileid="82" wangid="0,1,0,1,0,1,0,1"/>
   </wangset>
  </wangsets>
