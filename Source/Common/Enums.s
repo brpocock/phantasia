660305f9d8679fbed59e2520ecb0f71e7b86dbbe
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-04-06 12:27:47 -0400

    actions and attributes

--- Source/Common/Enums.s
+++ Source/Common/Enums.s
@@ -19,17 +19,51 @@
 ;;; 
           ControllerJoy2b = $01
           Controller7800 = $00
-
-
-          NumMapRows = 13
+;;; 
+          NumMapRows = 13       ; on screen at once
           MaxSprites = 16
-
+;;; 
           PlayerMovementSpeed = $40
 
           PlayerFacingUp = 4 * 8 * 4
           PlayerFacingLeft = 4 * 8 * 5
           PlayerFacingRight = 4 * 8 * 6
           PlayerFacingDown = 4 * 8 * 7
-
+;;; 
           ActionIdle = $00
           ActionWalking = $01
+          ActionFlying = $02
+          ActionSwimming = $03
+          ActionUseEquipment = $04
+;;; 
+          ;; Byte 0
+          AttrWallNorth = $01
+          AttrWallSouth = $02
+          AttrWallWest = $04
+          AttrWallEast = $08
+          AttrWallUnderNorth = $10
+          AttrWallUnderSouth = $20
+          AttrWallUnderWest = $40
+          AttrWallUnderEast = $80
+          ;; Byte 1
+          ;; XXX what is in bit $01 here? ceiling?
+          AttrWade = $02
+          AttrSwim = $04
+          AttrClimb = $08
+          AttrPit = $10
+          AttrDoor = $20
+          AttrFlammable = $40
+          AttrStairsDown = $80
+          ;; Byte 2
+          AttrIce = $01
+          AttrFire = $02
+          AttrTriggerMask = $0c
+          AttrStepTrigger = $04
+          AttrPullTrigger = $08
+          AttrPushTrigger = $0c
+          AttrIron = $10
+          AttrPushMask = $60
+          AttrPush = $20
+          AttrPushHeavy = $40
+          AttrPushVeryHeavy = $60
+          AttrExit = $80
