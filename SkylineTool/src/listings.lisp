2e35b9952cff552606d0c05f95011903dd43088f
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-07-22 19:01:19 -0400

    hash and make case-insensitive labels

--- SkylineTool/src/listings.lisp
+++ SkylineTool/src/listings.lisp
@@ -4,16 +4,20 @@
   "Converts LABELS-FILE into a format MAME can read as “comments” in MAME-FILE."
   (with-output-to-file (mame mame-file :if-exists :supersede)
     (with-input-from-file (labels labels-file)
-      (loop for line = (read-line labels nil nil)
-            while line
-            do (destructuring-bind (label address) (mapcar (lambda (el)
-                                                             (string-trim " " el))
-                                                           (split-sequence #\= line))
-                 (when-let (addr (cond ((find #\$ address)
-                                        (parse-integer (string-trim "$" address) :radix 16))
-                                       ((every #'digit-char-p address)
-                                        (parse-integer address))
-                                       (t nil)))
-                   (format mame "// ~8,'0x, ~a~%" addr (string-trim " " label))))))
+      (let ((comments (make-hash-table)))
+        (loop for line = (read-line labels nil nil)
+              while line
+              do (destructuring-bind (label address) (mapcar (lambda (el)
+                                                               (string-trim " " el))
+                                                             (split-sequence #\= line))
+                   (when-let (addr (cond ((find #\$ address)
+                                          (parse-integer (string-trim "$" address) :radix 16))
+                                         ((every #'digit-char-p address)
+                                          (parse-integer address))
+                                         (t nil)))
+                     (setf (gethash addr comments) (cffi:translate-camelcase-name label)))))
+        (loop for addr being the hash-keys of comments
+              for label = (gethash addr comments)
+              do (format mame "comadd ~8,'0x, ~a~%" addr (string-trim " " label)))))
     (format mame "printf \"\\n\\n\\n\\n\\nLabels loaded.\\nReady.\"")))
 
